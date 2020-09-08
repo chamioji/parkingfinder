23339d1b3be3cfbe82ba998babaf2122d0a41f0b
chamioji <chamioji@gmail.com>
2020-09-08 12:25:13 +0900

    [Add]オーナーサイト - パーキング編集

--- routes/owners.js
+++ routes/owners.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 
 const checkLogin = (req, res, next) => {
   if (!req.user) {
-      return res.redirect('../owners/login');
+    return res.redirect('/owners/login');
   }
   return next();
 };
@@ -13,27 +13,53 @@ const checkLogin = (req, res, next) => {
 router.get('/', checkLogin, (req, res, next) => {
   db.Parking.findAll({
     where: { ownerId: req.user.id}
-  }).then(parkings => {
-    var data = {
-      title: 'Parking Finder',
-      user: req.user,
-      content: parkings
-    };
-    res.render('owners/index', data);
-  });
+  })
+    .then(parkings => {
+      var data = {
+        title: 'Parking Finder',
+        user: req.user,
+        content: parkings
+      };
+      res.render('owners/index', data);
+    });
 });
 
 router.get('/show/:id', checkLogin, (req, res, next) => {
-  db.Parking.findOne({
-    where: { id: req.params.id}
-  }).then(parkings => {
-    var data = {
-      title: 'Parking Finder',
-      user: req.user,
-      content: parkings
-    };
-    res.render('owners/show', data);
-  });
+  db.Parking.findByPk(req.params.id)
+    .then(parkings => {
+      var data = {
+        title: 'Parking Finder',
+        user: req.user,
+        content: parkings
+      };
+      res.render('owners/show', data);
+    });
+});
+
+router.get('/edit/:id', checkLogin, (req, res, next) => {
+  db.Parking.findByPk(req.params.id)
+    .then(parkings => {
+      var data = {
+        title: 'Parking Finder',
+        user: req.user,
+        form: parkings,
+      };
+      res.render('owners/edit', data);
+    });
+});
+
+router.post('/edit/:id', checkLogin, (req, res, next) => {
+  db.Parking.findByPk(req.params.id)
+    .then(parking => {
+      parking.status = req.body.status;
+      parking.name = req.body.name;
+      parking.postcode = req.body.postcode;
+      parking.address = req.body.address;
+      parking.capacity = req.body.capacity;
+      parking.price = req.body.price;
+      parking.save()
+      res.redirect('/owners/show/' + req.params.id);
+    });
 });
 
 router.get('/login', (req, res) => {
@@ -45,14 +71,14 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/login', passport.authenticate('local', {
-  successRedirect: '../owners',
-  failureRedirect: '../owners/login',
+  successRedirect: '/owners',
+  failureRedirect: '/owners/login',
   session: true
 }));
 
 router.get('/logout', (req, res) => {
   req.logout();
-  res.redirect('../owners/login');
+  res.redirect('/owners/login');
 });
 
 module.exports = router;
